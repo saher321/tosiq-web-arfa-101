9f4118476ada0e925a48e84776137c0d69edf6f5
pny <pnymeet@gmail.com>
2025-12-23 15:01:52 +0500

    Filtered users data

---
+++ web/assets/js/filter.js
@@ -0,0 +1,64 @@
+var allUsers = [
+    { name: "John", email: "john@email.coom", role: "Admin" },
+    { name: "Carlos", email: "carlos@email.coom", role: "User" },
+    { name: "Samantha", email: "samantha@email.coom", role: "Vendor" },
+    { name: "Jack", email: "jack@email.coom", role: "Admin" },
+    { name: "Abdullah", email: "abdullah@email.coom", role: "User" },
+];
+fetchAllUsers();
+
+function fetchAllUsers() {
+    var userRows = document.getElementById("tbody");
+    var mydata = ``;
+
+    allUsers.map( (user, i) => {
+        mydata += `
+        <tr>
+            <td>${i+1}</td>
+            <td>${user.name}</td>
+            <td>${user.email}</td>
+            <td>${user.role}</td>
+        </tr>
+        `;
+    });
+    userRows.innerHTML = mydata;
+
+}
+
+function getRole() {
+    var role = document.getElementById("role").value;
+    var userRows = document.getElementById("tbody");
+    var mydata = ``;
+    console.log(role);
+    var selectedValue = capitalizeFirstLetter(role);
+
+    if (role === "all") {
+        fetchAllUsers();
+        return;
+    }
+
+    const selectedUsers = allUsers.filter( (user) => { 
+        return user.role == selectedValue 
+    });
+
+    selectedUsers.map( (user, i) => {
+        mydata += `
+        <tr>
+            <td>${i+1}</td>
+            <td>${user.name}</td>
+            <td>${user.email}</td>
+            <td>${user.role}</td>
+        </tr>
+        `;
+    });
+    userRows.innerHTML = mydata;
+    // userRows.innerHTML = mydata;
+}
+
+
+function capitalizeFirstLetter(string) {
+  if (!string) {
+    return ""; // Handle empty strings safely
+  }
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
--- web/assets/js/webpageTheme.js
+++ web/assets/js/webpageTheme.js
@@ -1,5 +1,12 @@
 applyTheme();
 
+// will runs on first input (background)
+function showbgColor() {
+    var bgColor = document.getElementById("bg-color").value
+    document.body.style.backgroundColor = bgColor;
+    localStorage.setItem("bg-color", bgColor)
+}
+
 function applyTheme() {
     var bgColor = localStorage.getItem("bg-color")
     var txtColor= localStorage.getItem("txt-color")
@@ -9,7 +16,7 @@ function applyTheme() {
         alert("Colors not provided");
         return;
     }
-    
+
     document.body.style.backgroundColor = bgColor;
     document.body.style.color = txtColor;
 }
