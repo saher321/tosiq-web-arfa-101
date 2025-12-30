409bb46e3bd09c44efa529ed095bb59e9a9a2be0
pny <pnymeet@gmail.com>
2025-12-30 15:48:29 +0500

    Digital clock

--- web/assets/js/more.js
+++ web/assets/js/more.js
@@ -18,63 +18,56 @@
 // greetings("Asad", "Evening");
 
 // function greetings () {
-    
+
 // }
 // function greetings (name) {
-    
+
 // }
 
 // function greetings (name, timeOfDay) {
 //     if (!name && !timeOfDay) return console.log("Simple function calling");
 
 //     if (name && !timeOfDay) return console.log(`Hello ${name}`);
-    
+
 //     if (!name && timeOfDay) return console.log(`Good ${timeOfDay}`);
 
 //     if (name && timeOfDay) return console.log(`Hello ${name}, Good ${timeOfDay}`);
 // }
 
-
 /// ************* CLASS ***************
 
 class Cars {
+  constructor(make, model) {
+    this.make = make;
+    this.model = model;
+  }
 
-    constructor(make, model) {
-        this.make = make;
-        this.model = model;
-    }
-
-    getDetails () {
-        console.log(`My car name is ${this.make} has model number is ${this.model}`)
-    }
+  getDetails() {
+    console.log(
+      `My car name is ${this.make} has model number is ${this.model}`
+    );
+  }
 }
 
 class Toyota extends Cars {
+  constructor(make, model, variant) {
+    super(make, model);
+    this.variant = variant;
+  }
 
-    constructor(make, model, variant) {
-        super(make, model)
-        this.variant = variant;
-    }
-
-    getDetails () {
-        console.log(`My car name is ${this.make} has model number is ${this.model + 1} with the variant of ${this.variant}`);     
-    }
+  getDetails() {
+    return `My car name is ${this.make} has model number is ${this.model + 1} with the variant of ${this.variant}`;
+  }
 }
 
 // let c = new Cars("Toyota", 2007);
 // console.log(c.getDetails())
 
-let t = new Toyota("Toyota Camry", 2007, "2.0cc");
-console.log(t.getDetails())
-
-
-
-
-
-
-
-
-
-
-
-
+let t1 = new Toyota("Toyota Camry", 2007, "2.0cc");
+let t2 = new Toyota("Toyota 2.0", 2005, "1.5cc");
+let t3 = new Toyota("Toyota Altis", 2017, "1.6cc");
+let t4 = new Toyota("Toyota Grande", 2020, "1.8cc");
+console.log(t1.getDetails());
+console.log(t2.getDetails());
+console.log(t3.getDetails());
+console.log(t4.getDetails());
