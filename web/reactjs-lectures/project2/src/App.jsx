653df147f3758d41b2a95ae310160138c3337c56
pny <pnymeet@gmail.com>
2026-01-05 15:21:12 +0500

    useState hook

--- web/reactjs-lectures/project2/src/App.jsx
+++ web/reactjs-lectures/project2/src/App.jsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
 import Button from "./components/Button";
 
 const App = () => {
+  // const [ data, setData ] = useState(true)
+  const [myName, setMyName] = useState("Hello world")
+  // const data = true
   return (
     <>
       <h1>This is project 2 heading</h1>
+      {myName || "John Doe"}
+      {/* <button onClick={() => setData(false)}>Show buttons</button>
+      <br />
 
+      { data ?
+      <>
       <Button title="Button 1" />
       <Button title="Button 2" />
       <Button title="Button 3" />
+      </> : "No button found"
+      } */}
+
     </>
   )
 }
