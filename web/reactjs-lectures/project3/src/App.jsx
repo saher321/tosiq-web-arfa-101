87ae34a3e7482639b7d99d54082142cc78e66565
pny <pnymeet@gmail.com>
2026-01-06 14:46:25 +0500

    tailwind

---
+++ web/reactjs-lectures/project3/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+const App = () => {
+  return (
+    <>
+    {/* <h1 style={{color: "red", backgroundColor: "yellow"}} className='heading'>This is my app component</h1>       */}
+
+
+    <div className='text-2xl text-gray-600 bg-white w-fit p-3 m-4 rounded-lg border-l-2 border-l-lime-500'>
+        Hello Mike, Good evening!
+    </div>
+    </>
+  )
+}
+
+export default App
