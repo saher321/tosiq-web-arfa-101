d3d737a27b4ec31fa723f2b4f3b7d7aa1ef448ab
= <=>
2026-02-02 14:39:35 +0500

    avg of 5 subjects

---
+++ mywebsite/backend/index.php
@@ -0,0 +1,14 @@
+<?php 
+
+    $eng    = 39;
+    $math   = 89;
+    $urdu   = 76;
+    $com    = 100;
+    $sst    = 67;
+
+    $sum  = $eng + $math + $urdu + $com + $sst;
+    $avg  = $sum / 5;
+
+    echo "Average of five subject is $avg";
+    
+?>
