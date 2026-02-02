05d20eab4740fcc3d9041ac75ff8eba9f270a5ad
= <=>
2026-02-02 16:02:32 +0500

    php more

--- mywebsite/backend/index.php
+++ mywebsite/backend/index.php
@@ -1,14 +1,33 @@
 <?php 
 
-    $eng    = 39;
-    $math   = 89;
-    $urdu   = 76;
-    $com    = 100;
-    $sst    = 67;
+    $username   = $_GET["username"];
+    $age        = $_GET["age"];
+    // $email      = $_GET["email"];
+    // $password   = $_GET["password"];
 
-    $sum  = $eng + $math + $urdu + $com + $sst;
-    $avg  = $sum / 5;
+    if (!$username || !$age) {
+        header("Location: ./home.php");
+        return;
+    }
 
-    echo "Average of five subject is $avg";
+    header("Location: ./success.html");
+    // echo "Your name: $username \n";
+    // echo "Your email: $email \n";
+    // echo "Your password: $password \n";
+
+    // $eng    = 39;
+    // $math   = 89;
+    // $urdu   = 76;
+    // $com    = 100;
+    // $sst    = 67;
+
+    // $sum  = $eng + $math + $urdu + $com + $sst;
+    // $avg  = $sum / 5;
+
+    // echo "Average of five subject is $avg";
     
+    // $fahrenite = 67;
+    // output : 19.4
+
+
 ?>
