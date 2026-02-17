86d5149768cafb9e4f902a8125217c0da309d343
= <pnymeet@gmail.com>
2026-02-17 14:51:24 +0500

    delete from table

--- mywebsite/webpages/staff_list.php
+++ mywebsite/webpages/staff_list.php
@@ -3,6 +3,17 @@
     include("../config/db.php");
 
     $staffs = mysqli_query($connection, "select * from staffs");
+
+
+    if (isset($_GET['id'])) {
+        $query = "delete from staffs where id=". $_GET['id'];
+
+        mysqli_query($connection, $query);
+
+        // unlink();
+
+        header('Location: ./staff_list.php');
+    }
 ?>
 
 
@@ -14,6 +25,8 @@
     <title>Document</title>
 </head>
 <body>
+
+<a href="./staff_create.php">Add Staff</a>
 
 <table border="1" cellpadding="20" cellspacing="0">
     <thead>
@@ -30,7 +43,13 @@
     arra = [{}, {}]
     -->
     <tbody>
+        <?php if(mysqli_num_rows($staffs) == 0){
+        ?>
+            <tr>
+                <td colspan="5">No record were found</td>
+            </tr>
         <?php
+        } else {
             foreach($staffs as $staff){
         ?>
         <tr>
@@ -47,14 +66,34 @@
             </td>
             <td><?php echo $staff['name']?></td>
             <td><?php echo $staff['email']?></td>
-            <td>Edit / Delete</td>
+            <td>Edit /
+                <!-- <a href="./staff_list.php?id=
+                 <php echo $staff['id']?>">
+                    Delete
+                </a> -->
+                <a href="#" onclick="del(<?php echo $staff['id']?>)">Delete</a>
+            </td>
         </tr>
         <?php 
+
             }
+        }
         ?>
     </tbody>
 
 </table>
+
+<!-- js -->
+ <script>
+    function del(id){
+        if(confirm("Delete this record?")){
+            window.location = `./staff_list.php?id=${id}`;
+        } else {
+            return;
+        }
+    }
+ </script>
+
 
 </body>
 </html>
